7545d107b91da11eefd431d29d49c2cd297d23f0
hanzhizhong <kf_hz@sina.com>
2019-12-03 00:16:27 +0800

    托盘图标

---
+++ tray/index.js
@@ -0,0 +1,31 @@
+const {app,BrowserWindow}=require('electron');
+const path=require('path')
+let win;
+function createWin(){
+    win=new BrowserWindow({
+        width:1000,
+        height:500,
+        show:false,
+        webPreferences:{nodeIntegration:true}
+    });
+    win.loadURL(path.join(__dirname,'views/index.html'))
+    win.on('ready-to-show',()=>{
+        win.show()
+    })
+    win.on('closed',()=>{
+        win=null
+    })
+}
+
+app.on('ready',createWin);
+app.on('window-all-closed',()=>{
+    if(process.platform!="drawin"){
+        app.quit()
+    }
+});
+
+app.on('activate',()=>{
+    if(win==null){
+        createWin()
+    }
+})
